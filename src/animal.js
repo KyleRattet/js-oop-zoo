e7ed3be427d1ddaec9ec29a6043dae345ae01983
KyleRattet <krattet@gmail.com>
2015-08-05 16:35:04 -0600

    animal class methods complete, and all tests are passing

--- src/animal.js
+++ src/animal.js
@@ -14,21 +14,29 @@ Animal.prototype.oink = function(){
 };
 
 Animal.prototype.growUp = function(){
-  return this.age ++;
+  return this.age +1;
 };
 
 Animal.prototype.feed = function(){
   if (this.awake === true){
-    reutrn "NOM NOM NOM";
+    return "NOM NOM NOM";
+  } else {
+    return "sleeping, don't feed";
   }
 };
 
 Animal.prototype.wakeUp = function(){
-  return this.awake === true;
+  if (this.awake === false) {
+    this.awake = true
+  }
+  return this.awake;
 };
 
 Animal.prototype.sleep = function() {
-  return this.awake === false;
+  if (this.awake === true) {
+    this.awake = false;
+  }
+  return this.awake;
 };
 
 module.exports = Animal;
