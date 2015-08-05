7b59f2b84ceb2b4b6763a30927db6ba91339d722
KyleRattet <krattet@gmail.com>
2015-08-05 15:41:30 -0600

    added animal class methods:

--- src/animal.js
+++ src/animal.js
@@ -2,6 +2,7 @@ function Animal(name, age, kind){
   this.name = name;
   this.age = age;
   this.kind = kind;
+  this.awake = false;
 }
 
 Animal.prototype.oink = function(){
@@ -10,6 +11,24 @@ Animal.prototype.oink = function(){
   } else {
     return "I'm not a pig";
   }
+};
+
+Animal.prototype.growUp = function(){
+  return this.age ++;
+};
+
+Animal.prototype.feed = function(){
+  if (this.awake === true){
+    reutrn "NOM NOM NOM";
+  }
+};
+
+Animal.prototype.wakeUp = function(){
+  return this.awake === true;
+};
+
+Animal.prototype.sleep = function() {
+  return this.awake === false;
 };
 
 module.exports = Animal;
